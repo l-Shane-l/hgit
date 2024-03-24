750e61be6ae0b7245c957bd11bb465d5d20fec21
l-Shane-l <graainneog@proton.me>
2024-03-24 01:17:54 +0100

    stage 1

--- app/Main.hs
+++ app/Main.hs
@@ -10,13 +10,10 @@ import System.IO (IOMode (WriteMode), hPutStrLn, withFile)
 
 main :: IO ()
 main = do
-    -- You can use print statements as follows for debugging, they'll be visible when running tests.
-    putStrLn "Logs from your program will appear here"
-
-    -- Uncomment this block to pass first stage
-    -- let createParents = True
-    -- createDirectoryIfMissing createParents ".git"
-    -- createDirectoryIfMissing createParents (".git" </> "objects")
-    -- createDirectoryIfMissing createParents (".git" </> "refs")
-    -- withFile (".git" </> "HEAD") WriteMode $ \f -> hPutStrLn f "ref: refs/heads/main"
-    -- putStrLn $ "Initialized git directory"
+  -- Uncomment this block to pass first stage
+  let createParents = True
+  createDirectoryIfMissing createParents ".git"
+  createDirectoryIfMissing createParents (".git" </> "objects")
+  createDirectoryIfMissing createParents (".git" </> "refs")
+  writeFile (".git" </> "HEAD") ("ref: refs/heads/main" ++ "\n")
+  putStrLn "Initialized git directory"
